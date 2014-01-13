9613bd1ffe040e18d0c2ac605d941b932b38882f
smof <simon_moffatt@warpmail.net>
2014-01-13 20:27:33 +0000

    Added logging and query checking to createRoles

--- createRoles.js
+++ createRoles.js
@@ -1,18 +1,37 @@
-//Simon Moffatt (C) 2013 - Part of RolesCreator library
-//Creates business role JSON object that contains role names and unique user identifier
+//Simon Moffatt (C) 2013 - Part of RolesCreator library - https://github.com/smof/openIDM_roles_creator_extension
+//Creates shell role JSON object that contains role names and unique user identifiers in an array.
+//
+//Verb: GET
+//Args: sourceSystem - system to analyse (currently managed/user)
+//Args: sourceAttribute - attribute within sourceSystem that contains functional grouping.  Eg jobTitle
+//Args: userAttribute - unique user attribute within sourceSystem that will be used to populate user array.  Eg _id
+//
+//Returns:
+//{"roleName" : ["user1",...,"userN"]}
 
-roleNames = [];
-roles = {};
+//Logging
+logger.info("Endpoint Request {}", request);
 
-//Pull in args
-sourceSystem = request.params['sourceSystem'];
+//Make sure the request coming in is a read/GET
+if (request.method !== "query") {
+    throw { 
+        "openidmCode" : 403, 
+        "message" : "Access denied"
+    } 
+}
+
+//Variable declaration
+var roleNames = [];
+var roles = {};
+
+//Pull in args given via URL
+sourceSystem = request.params['sourceSystem'] || "managed/user"; //default to managed/user
 sourceAttribute = request.params['sourceAttribute'];
 userAttribute = request.params["userAttribute"];
 
 //Query
 //users = openidm.query("managed/user", { "_queryId" : "query-all"});
 users = openidm.query(sourceSystem, { "_queryId" : "query-all"});
-
 
 //Get a list of unique role names based on chosen system attribute ///////////////////////////////////////////////////////////////////////////////////
 
